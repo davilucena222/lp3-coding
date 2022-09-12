43db88555b81ce9d32c98262af9585a1eadd0f35
Davi Luiz <davilucena222@gmail.com>
2022-09-12 19:32:52 +0000

    correção do exercício 1

--- atividades-nota/ex1/lib/ex1.ex
+++ atividades-nota/ex1/lib/ex1.ex
@@ -10,32 +10,32 @@ defmodule Ex1 do
 
   @doc "Multiplica o numero passado como parametro por 5."
   def mult5(x) do
-    nil
+    x * 5
   end
 
   @doc "Calcula o quadrado do numero x"
   def quadrado(x) do
-    nil
+    x * x
   end
 
   @doc "Calcula a soma dos quadrados de x e y: x ao quadrado + y ao quadrado."
   def soma_quadrados(x, y) do
-    nil
+    (x ** 2) + (y ** 2)
   end
 
   @doc "Concatena as duas strings a e b."
   def concat(a, b) do
-    nil
+    a <> b
   end
 
   @doc "Efetua a negacao logica (NOT) do booleano a."
   def negacao(a) do
-    nil
+    not a
   end
 
   @doc "Efetua o E-logico (AND) dos dois booleanos a e b."
   def e_logico(a, b) do
-    nil
+    a and b
   end
 
   ### Parte 2 - Funcoes recursivas com condicao de parada simples
@@ -46,31 +46,65 @@ defmodule Ex1 do
 
   @doc "Multiplica os numeros a e b usando apenas soma repetida (a >= 0, b >= 0)."
   def mult(a, b) do
-    nil
+    if b == 0 do
+      0
+    else
+      a + mult(a, b - 1)
+    end
   end
 
   @doc "Eleva o numero n `a potencia m, usando multiplicacao repetida (n >= 0, m >= 0)."
   def exp(n, m) do
-    nil
+    if m == 0 do
+      1
+    else
+      n * exp(n, m - 1)
+    end
   end
 
   @doc "Concatena a string s com si mesma n vezes, n >= 0"
   def replica(s, n) do
-    nil
+    if n == 0 do
+      ""
+    else
+      s <> replica(s, n - 1)
+    end
   end
 
   @doc "Determina se o numero n (n >= 0) e' par, usando apenas subtracao"
   def numero_par(n) do
-    nil
+    if n == 0 do
+      true
+    else
+      not numero_par(n - 1)
+    end
   end
 
   @doc "Determina se o numero n (n >= 0) e' impar, usando apenas subtracao"
   def numero_impar(n) do
-    nil
+    if n == 0 do
+      false
+    else
+      not numero_impar(n - 1)
+    end
   end
 
   # Bonus: tente definir numero_par usando numero_impar, e vice-versa
+  def n_par(n) do
+    if n == 0 do
+      true
+    else
+      n_impar(n - 1)
+    end
+  end
 
+  def n_impar(n) do
+    if n == 0 do
+      false
+    else
+      n_par(n - 1)
+    end
+  end
 
   ### Parte 3 - Funcoes recursivas com outras condicoes de parada
 
@@ -79,13 +113,17 @@ defmodule Ex1 do
   # deve ser pensada com cuidado
 
   @doc "Calcula a divisao inteira de a por b (a > 0 e b > 0), usando apenas subtracao"
-  def divide(a, b) do
-    nil
+  def divide(a, b) do # (20, 10)
+    if a < b do
+      0
+    else
+      divide(a - b, b) + 1
+    end
   end
 
   @doc "Calcula qual e' a menor potencia de 2 maior ou igual a n (n > 0)."
   def teto_pot2(n) do
-    nil
+    teto_pot2_aux(n, 1)
   end
 
   # Dica: crie uma funcao auxiliar de dois parametros, o primeiro e' o mesmo n
@@ -93,5 +131,13 @@ defmodule Ex1 do
   # na condicao de parada e na chamada recursiva; retorne o primeiro valor do
   # segundo parametro que e' maior que n. Use a funcao auxiliar para implementar
   # teto_pot2.
+
+  defp teto_pot2_aux(n , p) do
+    if p >= n do
+      p
+    else
+      teto_pot2_aux(n, p * 2)
+    end
+  end
 
 end
