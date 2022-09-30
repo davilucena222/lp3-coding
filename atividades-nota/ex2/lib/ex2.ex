dc4f028cab7e40114ceddda664a04ceb46931613
Davi Luiz <davilucena222@gmail.com>
2022-09-30 19:33:30 +0000

    answerin last question

--- atividades-nota/ex2/lib/ex2.ex
+++ atividades-nota/ex2/lib/ex2.ex
@@ -29,10 +29,13 @@ defmodule Ex2 do
   # e efetuar todas as multiplicacoes.
 
   @doc "Retorna uma string que e' a concatenacao de todas as strings na lista ls."
-  def concat_lista(ls), do: nil
+  def concat_lista([]), do: ""
+  def concat_lista([s | r]), do: s <> concat_lista(r)
 
   @doc "Retorna o maior numero em uma lista de numeros (nil se a lista for vazia)."
-  def max_lista(l), do: nil
+  def max_lista([]), do: nil
+  def max_lista([x]), do: x
+  def max_lista([x | r]), do: max(x, max_lista(r))
 
   # As vezes podemos querer uma parte dos elementos da lista, sejam
   # os primeiros ou ultimos.
@@ -45,7 +48,9 @@ defmodule Ex2 do
   iex> Ex2.primeiros_n([1, 2, 3, 4], 2)
   [1, 2]
   """
-  def primeiros_n(l, n), do: nil
+  def primeiros_n([], n), do: []
+  def primeiros_n(_, 0), do: []
+  def primeiros_n([x | r], n), do: [x | primeiros_n(r, n - 1)]
 
   @doc """
   Retorna os ultimos n elementos da lista l.
@@ -55,7 +60,11 @@ defmodule Ex2 do
   iex> Ex2.ultimos_n([1, 2, 3, 4], 2)
   [3, 4]
   """
-  def ultimos_n(l, n), do: nil
+  def ultimos_n(l, n), do: remove_primeiros_n(l, max(0, length(l) - n))
+
+  def remove_primeiros_n([], n), do: []
+  def remove_primeiros_n(l, 0), do: l
+  def remove_primeiros_n([x | r], n), do: remove_primeiros_n(r, n - 1)
 
   # Em algumas situacoes, queremos transformar cada elemento de uma lista da
   # mesma forma, obtendo uma nova lista com os resultados.
@@ -68,17 +77,19 @@ defmodule Ex2 do
   iex> Ex2.dobro_lista([1, 2, 3, 4])
   [2, 4, 6, 8]
   """
-  def dobro_lista(l), do: nil
+  def dobro_lista([]), do: []
+  def dobro_lista([x | r]), do: [2 * x | dobro_lista(r)]
 
   @doc """
   Dada uma lista de numeros, retorna uma nova lista com os numeros da lista
   original elevados ao quadrado.
 
   Exemplo:
-  iex> Ex2.dobro_lista([1, 2, 3, 4])
+  iex> Ex2.quadrado_lista([1, 2, 3, 4])
   [1, 4, 9, 16]
   """
-  def quadrado_lista(l), do: nil
+  def quadrado_lista([]), do: []
+  def quadrado_lista([x | r]), do: [x * x | quadrado_lista(r)]
 
   # Uma outra necessidade comum e' selecionar apenas os elementos de uma lista
   # que possuem alguma propriedade, por exemplo apenas os numeros positivos, ou
@@ -92,10 +103,16 @@ defmodule Ex2 do
   iex> Ex2.positivos_lista([-2, 2, 0, 88, 15, -11, 42])
   [2, 88, 15, 42]
   """
-  def positivos_lista(l), do: nil
+  def positivos_lista([]), do: []
+  def positivos_lista([x | r]) when x > 0, do: [x | positivos_lista(r)]
+  def positivos_lista([_ | r]), do: positivos_lista(r)
+
 
   @doc "Retorna apenas os numeros pares de uma lista de numeros."
-  def pares_lista(l), do: nil
+  def pares_lista([]), do: []
+  def pares_lista([x | r]) when rem(x, 2) == 0, do: [x | pares_lista(r)]
+  def pares_lista([_ | r]), do: pares_lista(r)
+
 
   # Podemos querer fazer operacoes no agregado, combinando os elementos
   # de mesma posicao em duas listas, gerando uma terceira lista. Por exemplo,
@@ -111,7 +128,9 @@ defmodule Ex2 do
   iex> Ex2.soma_listas([1, 2, 3], [7, 8, 9, 3])
   [8, 10, 12]
   """
-  def soma_listas(l1, l2), do: nil
+  def soma_listas([], l2), do: []
+  def soma_listas(l1, []), do: []
+  def soma_listas([x1 | r1], [x2 | r2]), do: [x1 + x2 | soma_listas(r1, r2)]
 
   @doc """
   Dadas duas listas de strings l1 e l2, retorna uma lista em que cada elemento
@@ -122,7 +141,9 @@ defmodule Ex2 do
   iex> Ex2.concat_listas(["a", "b", "c"], ["x", "y", "z"])
   ["ax", "by", "cz"]
   """
-  def concat_listas(l1, l2), do: nil
+  def concat_listas([], l2), do: []
+  def concat_listas(l1, []), do: []
+  def concat_listas([x1 | r1], [x2 | r2]), do: [x1 <> x2 | concat_listas(r1, r2)]
 
   # Algumas outras manipulacoes de listas.
 
@@ -138,7 +159,9 @@ defmodule Ex2 do
   iex> Ex2.append([1, 2, 3], [4, 5, 6])
   [1, 2, 3, 4, 5, 6]
   """
-  def append(l1, l2), do: nil
+  def append([], l2), do: l2
+  def append(l1, []), do: l1
+  def append(x1, x2), do: x1 ++ x2
 
 
   # Dica: para a funcao a seguir, pode ser necessario definir uma funcao auxiliar.
@@ -151,6 +174,5 @@ defmodule Ex2 do
   iex> Ex2.posicoes_pares([2, 4, 6, 8, 10])
   [4, 8]
   """
-  def posicoes_pares(l), do: nil
-
-end
+  def posicoes_pares([]), do: []
+  #def posicoes_pares(l), do: [l + 2 | posicoes_pares(l)]
